086dbec67475518e4b00d2c96535502ff922f83a
Nonolism <70848785+nonolism@users.noreply.github.com>
2024-09-21 19:59:36 -0300

    Adicionar função Sorteio

--- js/app.js
+++ js/app.js
@@ -3,22 +3,46 @@
 // Quando clicar em reinicar, tudo será zerado.
 
 let listaAmigos = [];
+let listaPares = [];
 
 function adicionar(){
     let campoNome = document.getElementById('nome-amigo');
-    let nome = campoNome.value;
     let campoAmigos = document.getElementById('lista-amigos');
     
-    if (nome != ''){
-        listaAmigos.push(` ${nome}`)
+    if (campoNome.value != ''){
+        listaAmigos.push(campoNome.value);
         campoAmigos.innerText = `${listaAmigos}`;
+        campoNome.value = '';
     } else {
         alert('Insira o nome de alguém antes de adicionar');
     }
 }
 
 function sortear(){
+    if (listaAmigos.length > 1){
+        embaralha(listaAmigos);
+        let listaSorteio = document.getElementById('lista-sorteio');
+        for (let i = 0; i < listaAmigos.length; i++){
+            if (i == listaAmigos.length - 1){
+                listaSorteio.innerHTML = listaSorteio.innerHTML +  listaAmigos[i] + ' --> ' + listaAmigos[0] + '<br>';
+            } else {
+                listaSorteio.innerHTML = listaSorteio.innerHTML +  listaAmigos[i] + ' --> ' + listaAmigos[i+1] + '<br>';
+            } 
+        }
+    } else if (listaAmigos.length == 1){
+        alert('Você não pode sortear a lista com apenas uma pessoa.');
+    } else{
+        alert('Você não pode sortear com a lista vazia.');
+    }
+}
 
+function embaralha(lista) {
+    let indice = lista.length;
+    while(indice) {
+        const indiceAleatorio = Math.floor(Math.random() * indice--);
+        [lista[indice], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice]];
+    }
 }
 
 function reinicar(){
